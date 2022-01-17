a76fe866a92982f72f119df1a5f1eec105c0d7b5
Jessica Fuller <64118777+gitnubl33t@users.noreply.github.com>
2022-01-17 20:41:21 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+/*  NOTES FROM JEN -
+Once again, because Grid allows overlapping cells, you can do all kinds of interesting layout and special effects.
+
+Here, I've used Grid to stack two figures on top of each other. Stacking may be controlled via z-index.
+
+Then I used some simple CSS animation to fade the top kitten in and out. 
+
+The keyframes are set up to show the image as is 25% of the interval, fade from one image to the other between 25-75% of the time interval, and show the last image the final 25% of the interval. Then the animation rocks back the other way to fade in the kitten again.
+
+*/
